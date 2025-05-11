33b2c62c0d9dda04ab007c3791f1aa32535de9e6
John Abraham <137336636+JohnJohnAbraham@users.noreply.github.com>
2025-05-11 13:50:14 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from Preview Deploy App</h1>
+      <p>This is a simple React app deployed using CircleCI and Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
